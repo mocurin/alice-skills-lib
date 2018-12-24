b3a8fff4ef2dc902067a07570f583db5726cc4db
Антон <43576696+mocurin@users.noreply.github.com>
2018-12-25 00:43:00 +0300

    Update source.cpp

--- source/source.cpp
+++ source/source.cpp
@@ -171,7 +171,7 @@ void MyCallback(const Alice::Request& request, Alice::Response& response)
 int main()
 {
     Skill battleships;
-    battleships.SetCallback(MyCallback);
-    battleships.Run();
+    battleships.setCallback(MyCallback);
+    battleships.run();
     return 0;
 }
